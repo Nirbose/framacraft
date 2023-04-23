93059dacb3e255e13e6bbc04164b09c51bd3b300
Nirbose <nirbose2@gmail.com>
2023-04-24 01:35:15 +0200

    new struct project

--- src/main/java/fr/nirbose/framacraft/Framacraft.java
+++ src/main/java/fr/nirbose/framacraft/Framacraft.java
@@ -1,14 +1,12 @@
 package fr.nirbose.framacraft;
 
-import fr.nirbose.framacraft.resources.commands.FramaCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Framacraft extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        this.getCommand("framacmd").setExecutor(new FramaCommand());
-
+        //
     }
 
     @Override
